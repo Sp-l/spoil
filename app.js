295bae7ca9a66495cfb40938198319e3c7279c38
54689053 <grahamburleigh2022@u.northwestern.edu>
2019-11-13 13:36:59 -0600

    Add styling, framework for addItem form

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var itemList = [];
+var __openStatus__ = false;
 
-function add_Item() {
-    window.confirm("Would you like to add an item?");
+function __init__() {
+    document.getElementById('foodForm').style.display = "none";
 };
 
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+    if (__openStatus__ == false) {
+        document.getElementById('foodForm').style.display = "block";
+    }
+    else {
+        console.log("Already open");
+    }
+};
+
+function closeForm() {
+    if (__openStatus__ == true) {
+        document.getElementById('foodForm').style.display = "none"
+    }
+};
+
+function check_empty() {
+    if (document.getElementById('name').value == "" || document.getElementById('date').value == "") {
+        alert("Fill All Fields !");
+    } 
+    else {
+        document.getElementById('foodForm').submit();
+        alert("Form Submitted Successfully");
+    }
+};
