c3df0f20bbeec6b7f0454b403e579b097d66133a
54689053 <grahamburleigh2022@u.northwestern.edu>
2019-11-12 23:51:11 -0600

    Added framework for add item form, added styling and html skeleton for main containers

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ var itemList = [];
 
 function add_Item() {
     window.confirm("Would you like to add an item?");
-}
+};
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
