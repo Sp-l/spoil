7ef967bbb67d1f5f5d89b35dd84f74e303fc8639
54689053 <grahamburleigh2022@u.northwestern.edu>
2019-11-19 19:41:37 -0600

    Added support for grocery list, more js helpers, FORM IS BROKEN

--- inventory_helpers.js
+++ inventory_helpers.js
@@ -1,4 +1,5 @@
 var itemList = [];
+var listCounter = 1;
 var __openStatus__ = false;
 
 function __init__() {
@@ -10,6 +11,7 @@ function openForm() {
     if (__openStatus__ == false) {
         document.getElementById('add-form').style.display = "block";
         console.log("Form opened");
+        __openStatus__ = true;
     }
     else {
         console.log("Already open");
@@ -20,10 +22,11 @@ function closeForm() {
     if (__openStatus__ == true) {
         document.getElementById('add-form').style.display = "none";
         console.log("Form closed");
+        __openStatus__ = false;
     }
 }
 
-function check_empty() {
+function checkEmpty() {
     if (document.getElementById('name').value == "" || document.getElementById('date').value == "") {
         alert("Fill All Fields !");
     } 
@@ -34,6 +37,20 @@ function check_empty() {
 }
 
 function getInput() {
+    var foodName = document.getElementById('food-name').value;
+    var expData = document.getElementById('expiration-date').value;
+    var foodGroup = document.getElementById('food-group').value;
+//    document.getElementById('food-list').style.display = "block";
+    console.log("Submitted!");
+}
+
+function addListItem() {
+    document.getElementById('list-name-'+listCounter).innerHTML = foodName;
+    document.getElementById('list-expiration-'+listCounter).innerHTML = expData;
+//    document.getElementById('group-img-'+listCounter).src = 
+}
+
+function findGroup() {
 
 }
 
